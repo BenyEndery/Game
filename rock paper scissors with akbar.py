ff35d5e1ae7faeaa4d2ee02a27200ac64f35dff9
BenFoxy <109299669+BenyEndery@users.noreply.github.com>
2023-02-28 21:59:39 +0330

    Add files via upload

---
+++ rock paper scissors with akbar.py
@@ -0,0 +1,44 @@
+me="akbar"
+a="rock"
+b="paper"
+c="scissors"
+lst=[]
+name=input("enter your name :")
+print("helloooo",name)
+print("my name is akbar! :)")
+print("i've game for you (:",name)
+print("rock,paper,scissors! (:")
+print("are you redy for game?",name)
+print("first you should pay 10$ :))))))))))))))))")
+print("its joke!")
+print("let's go!")
+print("choose one more and write it (:")
+print("rule of game!=pls write rock paper scissors (like Text)")
+game1=input("rock,paper,scissors ? :")
+if game1==a:
+     print("akbar :",b)
+elif game1 ==b:
+     print("akbar :",c)
+elif game1==c:
+     print("akbar :",a)
+print("ha ha im wiiiinnnneeeeerrr")
+print("don't worry")
+print("it's just a game")
+print("let's try again")
+game2=input("rock,paper,scissors ? :")
+if game2==a:
+     print(c)
+elif game2 ==b:
+     print(a)
+elif game2==c:
+     print(b)
+print("noooooooooooooooooo")
+print("why im loosing")
+print("im computer")
+print("im smart")
+print("how do you win this game? ):",name)
+print("i want revenge")
+print("but i tired")
+print("soooooooo")
+print("good byeeeeeeee",name)
+input()
