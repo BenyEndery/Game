e5b90507af686b54d8b079a844a13b5afcc7438f
BenFoxy <109299669+BenyEndery@users.noreply.github.com>
2023-02-28 21:57:26 +0330

    Add files via upload

---
+++ gol ya pooch with akbar.py
@@ -0,0 +1,89 @@
+lst1="por"
+lst2="khali"
+me="akbar"
+print("akbar:good morning!")
+print("im again here!")
+print("ummm")
+print("i don't remember your name!")
+print("can you write your name for me?!")
+game1 = input("yes or no? :")
+if game1 ==("yes"):
+    name=input("enter your name :")
+if game1 ==("no"):
+    print("whyyyyyy")
+if game1 ==("no"):
+    print("I told you my name")
+if game1 == ("no"):
+    print("if you dont say me I get angry and I don't want to play together anymore ):")
+if game1 ==("no"):
+    name=input("enter your name :")
+if game1==("no"or"yes"):
+    print("thank you",name)
+print("now we've gameeeeeeee (:")
+print("the name of game is gol ya pooch!")
+print("before we go in game")
+print("we've rule!")
+print("rule:pick one and write it ")
+print("let's gooooooo!")
+print("i start first")
+game2=input("right or left? :")
+if game2 == ("right"):
+    print("akbar:",lst1)
+if game2==("right"):
+    print("wow")
+    print("you are amazing!")
+if game2== ("left"):
+    print("akbar:",lst2)
+    print("ha ha ha ")
+    print("really you think you can win this game?!")
+print("*some one called akbar")
+print("oh")
+print("sorry , some one call me!")
+print("1 min")
+print("akbar:hello my love")
+print("akbar:oh , ok")
+print("akbar:milk,egg and fruit")
+print("akbar:ok my love i buy it")
+print("akbar:good bye")
+print("*call ended")
+print("hay")
+print("are you still here?")
+game3=input("yes or no? :")
+if game3==("yes"):
+    print("oh,good")
+if game3==("no"):
+    print("what!?")
+    print("So how are you writing?")
+print("i Should go somewhere!")
+print("But don't worry")
+print("my son is here!")
+print("you can play gol ya pooch with him!")
+print("ok , good byeee",name)
+print("...")
+print("mini akbar:hello")
+print("mini akbar:some one here?")
+game4=input("yes or no? :")
+if game4==("no"):
+        print("oh,ok")
+        print("bye")
+if game4==("yes"):
+    print("hello")
+    print("i think you are",name)
+    print("nice to meet you")
+    print("mini akbar:Zzz")
+    print("mini akbar:Zzz")
+    print(name,"i thing he sleep!")
+    for i in range(3):
+        game5=input("something say to mini akbar to wake up !:")
+        print("Zzz")
+    print(name,": oh god")
+
+
+    print("END")
+
+
+print("every think : benyamin")
+input()
+
+
+
